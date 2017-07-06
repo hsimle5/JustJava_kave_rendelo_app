856c3137d876828f30234cc493907db9b727f0d4
hsimle5 <hsimle@gmail.com>
2017-07-06 16:40:54 +0200

    ht

--- app/src/main/java/hu/flexisys/soma/justjava/MainActivity.java
+++ app/src/main/java/hu/flexisys/soma/justjava/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Spinner;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +17,7 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     public static final int COFFEE_PRICE = 2;
+    public static final int WHIPPED_CREAM_PRICE = 1;
     int quantity = 0;
 
     @Override
@@ -30,8 +31,14 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         int price;
-        price = calculatePrice(quantity,COFFEE_PRICE);
-        String message = createOrderSummary(price, "Soma" , quantity);
+        CheckBox whipped_cream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean isWhippedCream = whipped_cream.isChecked();
+        int oneCoffeePrice = COFFEE_PRICE;
+        if (isWhippedCream) oneCoffeePrice+=WHIPPED_CREAM_PRICE;
+        price = calculatePrice(quantity,oneCoffeePrice);
+        TextView nameText = (TextView) findViewById(R.id.name_text);
+        String name = nameText.getText().toString();
+        String message = createOrderSummary(price, name , quantity);
         displayMessage(message);
         coffeeToast();
     }
@@ -65,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int calculatePrice (int price, int quantity){
+
+
         return price*quantity;
     }
 
